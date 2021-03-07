b6b42952450816118864959848dbc8dfd7169f9c
rkrishnanal <rkrishnanal.it@gmail.com>
2021-03-07 22:51:23 +0300

    git

--- demo.py
+++ demo.py
@@ -1,2 +1,13 @@
-print ("hello world")
-print ("hello world new")
+#print ("hello world")
+#print ("hello world new")
+
+#python variables
+# is store date 
+# name and _ allows as vairable 
+salary = 10
+#date type
+print(type(salary))
+EMI = 20.005
+Paid = salary + EMI
+print(type(Paid))
+print(Paid)
