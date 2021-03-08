8c010e6c95ecb4d16d9471f941bd72452ec7e04c
rkrishnanal <rkrishnanal.it@gmail.com>
2021-03-08 23:46:55 +0300

    Today learnt

---
+++ Typeconversion.py
@@ -0,0 +1,15 @@
+x = 1
+y = 2.8 
+
+z= 1j
+
+print (type(x))
+A= float (x) #converstion interger to float
+print (type(A))
+print(A)
+
+print (y)
+B=complex(y)
+print (B)
+
+#Python Casting one data type to another data typer
---
+++ string.py
@@ -0,0 +1,25 @@
+Test = 'radha "python"'
+print (Test)
+test2 = "azureportal.com \"testing\" verison '3.5'"
+print (test2)
+
+#''' to run multiple line 
+a = ''' Active directory
+Vmware , azure'''
+print (a)
+
+#string operators
+# +
+m= 'radha'
+F = 'lakshmi'
+b = 'tanu'
+print (m + F + b)
+
+# * operator
+
+print (b * 10)
+
+# in operator
+
+s = 'radha'
+print ( s  not in "this the vadha lines")
