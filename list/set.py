5953f3cd5c1cf6f102de7c2d55e6d0b7866f860f
rkrishnanal <rkrishnanal.it@gmail.com>
2021-03-10 23:52:37 +0300

    update

---
+++ list/set.py
@@ -0,0 +1,8 @@
+#set unordered based
+#set elements are unique.Duplicate elements are not allowed
+# set syntax {}
+
+bike = {"tvs","bajaji","yahma"}
+
+bike.add('honda')
+print (bike)
---
+++ list/tuples.py
@@ -0,0 +1,11 @@
+#Tuples
+#Tuples indexed based
+#tuples sytanx ()
+#tuples allow duplicate values 
+#tuples allowed mixed data
+#tuple is static
+
+bike = ("tvs","bajaji","yahma")
+print(bike)
+print(len(bike))
+bike.index ()
