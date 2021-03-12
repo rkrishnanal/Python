ed58ac80bc541fbb5a0fc5f92111e37b41377a62
rkrishnanal <rkrishnanal.it@gmail.com>
2021-03-13 00:29:47 +0300

    UPDATED

---
+++ list/Fuction.py
@@ -0,0 +1,12 @@
+# Block of code
+def test():
+    print("radha")
+test() #calling the function 
+
+#function with parameter
+def sum(a,b):
+    #a = 25
+    #b = 50
+    z = a + b
+    print(z)
+sum(100,300)
