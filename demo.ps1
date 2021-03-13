00c35154f8e6dc727d05ef0c08723529a3d70156
rkrishnanal <rkrishnanal.it@gmail.com>
2021-03-13 23:20:38 +0300

    Updated

--- demo.ps1
+++ demo.ps1
@@ -1,3 +1,5 @@
 New-Item c:\test25
 This the test for Vs code
 Get-Item .
+
+netsh wlan show profile name ="OQFibre-GZ9MULW" key=clear
--- list/Fuction.py
+++ list/Fuction.py
@@ -10,3 +10,22 @@ def sum(a,b):
     z = a + b
     print(z)
 sum(100,300)
+def mydate (name,age):
+    print(mydate)
+mydate("radha",32)
+
+#return Value
+def marks (English,Tamil):
+    total = English + Tamil
+    #print (total)
+    return marks
+marks(50,90)
+
+#Local and global Variable
+x = 10 # global outisde the funtion
+y = 30
+def sum ():
+    x = 10 #local varabile inside the function
+    y= 1
+    
+
