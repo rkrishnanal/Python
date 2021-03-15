3a16322cf3f86fd4aaf08c821cca46f85e67a458
rkrishnanal <rkrishnanal.it@gmail.com>
2021-03-15 23:51:00 +0300

    Updated

---
+++ Number.py
@@ -0,0 +1,13 @@
+#integer 1.2.3
+#Float 1.4 ,1.5
+#Complex 5+2j
+# to find power *
+#print (3 ** 5)
+print (5//2) # // used round of to get reminder
+
+#VARIABLE a-z, _
+Bike_Model1,Model2="Splendorbs6","Splendor2020"
+#omapy="Hero"
+Price1,Price2= 50000,60000
+print(Bike_Model1,Price1,Model2,Price2)
+#print(type(Price))
