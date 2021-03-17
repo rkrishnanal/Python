066d51f7daf57ff504531b66c6589b88cf1b388e
rkrishnanal <rkrishnanal.it@gmail.com>
2021-03-17 22:58:26 +0300

    Update

--- Number.py
+++ Number.py
@@ -11,3 +11,21 @@ Bike_Model1,Model2="Splendorbs6","Splendor2020"
 Price1,Price2= 50000,60000
 print(Bike_Model1,Price1,Model2,Price2)
 #print(type(Price))
+
+#Boolean True or False
+
+#casting
+a=float(10)
+b=int(10.10)
+c=str(120)
+print(b)
+
+#type
+print(type(c))
+
+#list Syntax [] 
+number=[1,2,8,2,6]
+number.sort(reverse=True)
+print(number)
+
+#tuples (We cannot change the content)
