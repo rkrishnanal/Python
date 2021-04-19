484735b48e648d279ac88768c25b408d0d917a74
rkrishnanal <rkrishnanal.it@gmail.com>
2021-04-19 20:27:59 +0300

    test

---
+++ list/Operators.py
@@ -0,0 +1,15 @@
+# == Equals
+# != not equal
+#< less
+#> Greater than
+# >= Greater than or equal to
+# <=Less than or equal to
+
+Tani_Age = 3
+Age_at_kindergarden= 5
+print(Tani_Age == Age_at_kindergarden)
+import math
+print("pi is",math.pi)
+
+#test
+#branchgit 
